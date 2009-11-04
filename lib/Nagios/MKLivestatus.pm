65d6a28a95553aa19fa82518aa8547c4a1b6a2aa
sni <sven@nierlein.de>
2009-11-04 09:48:26 +0000

    added description
    git-svn-id: http://svn.nierlein.de/svn/perl/Nagios-MKLivestatus@17 0e3e1bab-e52c-46b4-ac39-a665d9bf5c4c

--- lib/Nagios/MKLivestatus.pm
+++ lib/Nagios/MKLivestatus.pm
@@ -99,53 +99,35 @@ sub selectall_arrayref {
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Nagios::MKLivestatus - Perl extension for blah blah blah
+    Nagios::MKLivestatus - Perl extension for accession data from check_mk - livestatus Nagios addon
 
 =head1 SYNOPSIS
 
-  use Nagios::MKLivestatus;
-  blah blah blah
+    use Nagios::MKLivestatus;
+    my $nl = Nagios::MKLivestatus->new({ socket => '/var/lib/nagios3/rw/livestatus.sock' });
+    my $hosts = $nl->selectall_arrayref("GET hosts");
 
 =head1 DESCRIPTION
 
-Stub documentation for Nagios::MKLivestatus, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
+    This module connects via socket to the check_mk livestatus nagios addon. You first have
+    to install and activate the livestatus addon in your nagios installation.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+    For more information see the Livestatus page: http://mathias-kettner.de/checkmk_livestatus.html
 
 =head1 AUTHOR
 
-root, E<lt>root@E<gt>
+    Sven Nierlein, E<lt>nierlein@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009 by root
+    Copyright (C) 2009 by Sven Nierlein
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.10.0 or,
-at your option, any later version of Perl 5 you may have available.
-
+    This library is free software; you can redistribute it and/or modify
+    it under the same terms as Perl itself.
 
 =cut
